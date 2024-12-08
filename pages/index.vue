dc46703f12f8abf64ce1154f0ba2c6e723d5ebfe
miftahpj <142276212+miftahpj@users.noreply.github.com>
2024-12-08 16:48:06 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,235 @@
+<template>
+<title>Beranda</title>
+    <div class="parallax">
+        <div class="text ">
+          <h1>
+            <div class="p-3 mb-2 bg-white text-dark bg-opacity-25 fw-bold display-5 row text-center text-md-center ">
+              Selamat Datang Di Website Resmi SMKN 4 Tasikmalaya
+            </div>
+          </h1>
+        </div>
+    </div>
+    
+<div class="container mt-5">
+   <div class="row">
+    <div class="col-lg-8">
+      <div id="carouselExampleDark" class="carousel carousel-dark slide ">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="/assets/2.jpg" class="d-block w-100 h-50 rounded" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="/assets/1.jpg" class="d-block w-100 h-50 rounded" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+    <div class="col-lg-4">
+
+    <h1>BERITA</h1>
+    <p class="placeholder-glow" style="width: 25%;">
+  <span class="placeholder col-12"></span>
+    </p>
+    <h3>PPDB</h3>
+    <h6>infomasi PPDB jawa Barat SMKN 4 Tasikmalaya tahun ajaran 2022/2023</h6>
+
+    <button class="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Tekan untuk info lebih lanjut</button>
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+      <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">PPDB SMKN 4 Tasikmalaya</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Try scrolling the rest of the page to see this option in action.</p>
+  </div>
+</div>
+
+    <h3>PPDB</h3>
+    <h6>infomasi PPDB jawa Barat SMKN 4 Tasikmalaya tahun ajaran 2022/2023</h6>
+    <button class="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Tekan untuk info lebih lanjut</button>
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">PPDB SMKN 4 Tasikmalaya</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Try scrolling the rest of the page to see this option in action.</p>
+  </div>
+</div>
+
+  </div>
+  
+  </div>
+</div>
+
+<div class="mt"></div>
+
+<div class="parallax2">
+
+<div class="text-center bg-transparent">
+  <div class="card-body">
+    <div class="p-3 mb-2 bg-white text-dark bg-opacity-25">
+      <img src="/assets/kepsek.png" alt="" width="10%" height="10%">
+    <h5 class="card-title">Kurniawan</h5>
+    <p class="card-text">Kepala SMKN 4 Tasikmalaya</p>
+    </div>
+  </div>
+</div>
+  <!-- <div class="kepala">
+    <div class="mx-auto p-2">
+    <img src="/assets/logo-smkn4.png" alt="" width="100">
+    <h5 class="card-title">Kurniawan</h5>
+    <p class="text">Kepala Smkn 4 Tasikmalaya</p>
+    </div>
+  </div> -->
+</div>
+
+<div class="mb"></div>
+
+<div class="container text-center card-center">
+  <div class="row">
+      <div class="card" style="width: 18rem;">
+        <img src="/assets/Jurusan/pplg.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">PPLG</h5>
+              <p class="card-text">Perkembangan Perangkat Lunak Dan Gim.</p>
+          </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="/assets/Jurusan/tjkt.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">TJKT</h5>
+              <p class="card-text">Teknik Jaringan Dan Telekomunikasi.</p>
+          </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="/assets/Jurusan/tbsm.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">TBSM</h5>
+              <p class="card-text">Teknik Bisnis Dan Sepeda Motor.</p>
+          </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="/assets/Jurusan/dkv.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Dkv</h5>
+              <p class="card-text">Deasin Kommonikasi Visual.</p>
+          </div>
+      </div>
+
+      <div class="card" style="width: 18rem;">
+        <img src="/assets/Jurusan/toi.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">TOI</h5>
+              <p class="card-text">teknik Otomasi Industri.</p>
+          </div>
+      </div>
+  </div>
+</div>
+
+<div class="footer">
+  <div class="container mt-5 text-start">
+  <div class="row">
+    <div class="col-lg-5">
+      <h3>Hubungin kami</h3>
+      <span>
+        SMKN 4 Tasikmalaya.
+      </span>
+      <br>
+        Jl.Depok Kel.Sukamenak Kec.Purbaratu
+    <br>
+        Kota Tasikmalaya 46196.
+      
+    </div>
+    <div class="col-lg-4">
+      <h3>Media Sosial</h3>
+    </div>
+    <div class="col">
+      <h3>Link External</h3>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+</template>
+<style>
+.parallax {
+  /* The image used */
+  background-image: url('/assets/1.jpg');
+
+  /* Full height */
+  height: 95vh;
+  width: 100%;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.parallax2{
+  background-image: url('/assets/2.jpg');
+
+/* Full height */
+height: 50vh;
+width: 100%;
+
+/* Create the parallax scrolling effect */
+background-attachment: fixed;
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+}
+
+.text h1{
+    font-size: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 15%;
+}
+
+.card{
+    margin-left: 5%;
+    margin-top: 3%;
+}
+
+.card-body{
+  padding: 5%;
+}
+.mt{
+  margin-top: 5%;
+}
+.mb{
+  margin-bottom: 5%;
+}
+.footer{
+  width: 100%;
+  background-color: #8a8a8a;
+}
+</style>
